112090270a221dfd8c4a66720d76d9eccbd202f5
Quinton Fults <quinton.fults.work@gmail.com>
2022-03-10 11:16:49 -0800

    code

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const chalk = require('chalk')
+// const chalk = require('chalk')
 
-console.log(chalk.blue('Hello World!'))
-console.log(chalk.bgYellow(chalk.red('WOW')))
+// console.log(chalk.blue('Hello World!'))
+// console.log(chalk.bgYellow(chalk.red('WOW')))
+
+const inquirer = require('inquirer')
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'username',
+    message: 'What is your username'
+  },
+  {
+    type: 'list',
+    name: 'game',
+    message: 'Which game is best game?',
+    choices: ['Elden Ring', 'Red Dead Redemption 2', 'Power Wash Simulator', 'Witcher 3', 'Halo 3']
+  }
+])
+  .then(answers => {
+    console.log(answers)
+  })
+  .catch(err => console.log(err))
