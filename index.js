57df856726c6c9b6e6f0865a36d069e2c0dde0b3
Quinton Fults <quinton.fults.work@gmail.com>
2022-03-10 11:42:54 -0800

    code

--- index.js
+++ index.js
@@ -3,22 +3,38 @@
 // console.log(chalk.blue('Hello World!'))
 // console.log(chalk.bgYellow(chalk.red('WOW')))
 
-const inquirer = require('inquirer')
+// const inquirer = require('inquirer')
 
-inquirer.prompt([
-  {
-    type: 'input',
-    name: 'username',
-    message: 'What is your username'
-  },
-  {
-    type: 'list',
-    name: 'game',
-    message: 'Which game is best game?',
-    choices: ['Elden Ring', 'Red Dead Redemption 2', 'Power Wash Simulator', 'Witcher 3', 'Halo 3']
-  }
-])
-  .then(answers => {
-    console.log(answers)
-  })
-  .catch(err => console.log(err))
+// inquirer.prompt([
+//   {
+//     type: 'input',
+//     name: 'username',
+//     message: 'What is your username'
+//   },
+//   {
+//     type: 'list',
+//     name: 'game',
+//     message: 'Which game is best game?',
+//     choices: ['Elden Ring', 'Red Dead Redemption 2', 'Power Wash Simulator', 'Witcher 3', 'Halo 3']
+//   }
+// ])
+//   .then(answers => {
+//     console.log(answers)
+//   })
+//   .catch(err => console.log(err))
+
+// let nums = [1, 2, 3, 4, 5]
+
+// for (const index in nums) {
+//   console.log(nums[index])
+// }
+
+// for (const num of nums) {
+//   console.log(num)
+// }
+
+// const name = 'John Doe'
+
+// for (const char of name) {
+//   console.log(char)
+// }
